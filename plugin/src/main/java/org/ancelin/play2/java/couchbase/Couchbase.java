17882867e30c4f9e727ce4510bf022fe2c50e7e0
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2013-05-27 23:44:19 +0200

    Optimize Java API

--- plugin/src/main/java/org/ancelin/play2/java/couchbase/Couchbase.java
+++ plugin/src/main/java/org/ancelin/play2/java/couchbase/Couchbase.java
@@ -17,14 +17,17 @@ public class Couchbase {
     private static String initMessage = "The CouchbasePlugin has not been initialized! Please edit your conf/play.plugins file and add the following line: '400:package org.ancelin.play2.couchbase.CouchbasePlugin' (400 is an arbitrary priority and may be changed to match your needs).";
     private static String connectMessage = "The CouchbasePlugin doesn't seems to be connected to a Couchbase server. Maybe an error occured!";
 
+    private static Map<String, CouchbaseAPI> bucketsCache = new HashMap<String, CouchbaseAPI>();
+
     public static Map<String, CouchbaseAPI> buckets() {
-        Map<String, CouchbaseAPI> buckets = new HashMap<String, CouchbaseAPI>();
-        Iterator<Tuple2<String,org.ancelin.play2.couchbase.Couchbase>> iterator = plugin.buckets().iterator();
-        while(iterator.hasNext()) {
-            Tuple2<String,org.ancelin.play2.couchbase.Couchbase> tuple = iterator.next();
-            buckets.put(tuple._1(), new CouchbaseAPI(tuple._2().client().get()));
+        if (bucketsCache.isEmpty()) {
+            Iterator<Tuple2<String,org.ancelin.play2.couchbase.Couchbase>> iterator = plugin.buckets().iterator();
+            while(iterator.hasNext()) {
+                Tuple2<String,org.ancelin.play2.couchbase.Couchbase> tuple = iterator.next();
+                bucketsCache.put(tuple._1(), new CouchbaseAPI(tuple._2().client().get()));
+            }
         }
-        return buckets;
+        return bucketsCache;
     }
 
     public static CouchbaseAPI bucket(String name) {
