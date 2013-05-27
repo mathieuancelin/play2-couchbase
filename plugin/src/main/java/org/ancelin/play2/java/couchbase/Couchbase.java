bada411723c2e7032e4409239b765a5971e1a71b
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2013-05-27 23:42:17 +0200

    Change Java API

--- plugin/src/main/java/org/ancelin/play2/java/couchbase/Couchbase.java
+++ plugin/src/main/java/org/ancelin/play2/java/couchbase/Couchbase.java
@@ -1,6 +1,5 @@
 package org.ancelin.play2.java.couchbase;
 
-import com.couchbase.client.CouchbaseClient;
 import org.ancelin.play2.couchbase.CouchbasePlugin;
 import play.Play;
 import play.api.PlayException;
@@ -18,35 +17,35 @@ public class Couchbase {
     private static String initMessage = "The CouchbasePlugin has not been initialized! Please edit your conf/play.plugins file and add the following line: '400:package org.ancelin.play2.couchbase.CouchbasePlugin' (400 is an arbitrary priority and may be changed to match your needs).";
     private static String connectMessage = "The CouchbasePlugin doesn't seems to be connected to a Couchbase server. Maybe an error occured!";
 
-    public static Map<String, CouchbaseClient> buckets() {
-        Map<String, CouchbaseClient> buckets = new HashMap<String, CouchbaseClient>();
+    public static Map<String, CouchbaseAPI> buckets() {
+        Map<String, CouchbaseAPI> buckets = new HashMap<String, CouchbaseAPI>();
         Iterator<Tuple2<String,org.ancelin.play2.couchbase.Couchbase>> iterator = plugin.buckets().iterator();
         while(iterator.hasNext()) {
             Tuple2<String,org.ancelin.play2.couchbase.Couchbase> tuple = iterator.next();
-            buckets.put(tuple._1(), tuple._2().client().get());
+            buckets.put(tuple._1(), new CouchbaseAPI(tuple._2().client().get()));
         }
         return buckets;
     }
 
-    public static CouchbaseClient bucket(String name) {
+    public static CouchbaseAPI bucket(String name) {
         Option<org.ancelin.play2.couchbase.Couchbase> opt = plugin.buckets().get(name);
         if (opt.isDefined()) {
-            return opt.get().client().get();
+            return new CouchbaseAPI(opt.get().client().get());
         }
         throw new PlayException("CouchbasePlugin Error", initMessage);
     }
 
-    public static CouchbaseClient defaultBucket() {
+    public static CouchbaseAPI defaultBucket() {
         Option<Tuple2<String,org.ancelin.play2.couchbase.Couchbase>> tuple2Option = plugin.buckets().headOption();
         if (tuple2Option.isDefined()) {
-            return tuple2Option.get()._2().client().get();
+            return new CouchbaseAPI(tuple2Option.get()._2().client().get());
         }
         throw new PlayException("CouchbasePlugin Error", connectMessage);
     }
 
     public static <T> T withCouchbase(F.Function<CouchbaseAPI, T> block) {
         try {
-            return block.apply(new CouchbaseAPI(defaultBucket()));
+            return block.apply(defaultBucket());
         } catch (Throwable throwable) {
             throw new RuntimeException(throwable);
         }
@@ -54,7 +53,7 @@ public class Couchbase {
 
     public static <T> T withCouchbase(String bucket, F.Function<CouchbaseAPI, T> block) {
         try {
-            return block.apply(new CouchbaseAPI(bucket(bucket)));
+            return block.apply(bucket(bucket));
         } catch (Throwable throwable) {
             throw new RuntimeException(throwable);
         }
