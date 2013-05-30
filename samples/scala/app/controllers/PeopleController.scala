395035385c397132338a19bbc8dbe0678450d1b3
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2013-05-30 21:05:46 +0200

    Add true sample

--- samples/scala/app/controllers/PeopleController.scala
+++ samples/scala/app/controllers/PeopleController.scala
@@ -4,38 +4,62 @@ import org.ancelin.play2.couchbase.CouchbaseController
 import play.api.mvc.{Action, Controller}
 import java.util.UUID
 import models.People
+import models.Peoples
+import models.Peoples._
+import scala.concurrent.Future
+import play.api.libs.json._
+import play.api.libs.iteratee.{Enumeratee, Enumerator}
+import java.util.concurrent.TimeUnit
+import play.api.libs.concurrent.Promise
 
 object PeopleController extends Controller with CouchbaseController {
 
+  val peopleEnumerator: Enumerator[List[People]] = Enumerator.generateM[List[People]](
+    Promise.timeout(Some, 500, TimeUnit.MILLISECONDS).flatMap( n => Peoples.findAll().map( Option( _ ) ) )
+  )
+
+  val jsonTransformer: Enumeratee[List[People], String] = Enumeratee.map { list =>
+    val arr = Json.stringify( Json.toJson( list ) )
+    "data: {\"peoples\":" + arr + "}\n\n"
+  }
+
   def index() = Action {
      Async{
-       PeopleController.findAll().map(peoples => Ok(views.html.all(peoples)))
+       Peoples.findAll().map(peoples => Ok(views.html.all(peoples)))
      }
+  }
+
+  def peoples() = Action {
+    Ok.feed( peopleEnumerator &> jsonTransformer ).as( "text/event-stream" )
   }
 
   def show(id: String) = Action {
     Async {
-       PeopleController.findById(id).map { maybePeople =>
-          maybePeople.map(people => Ok(views.html.show(people))).getOrElse(NotFound)
+      Peoples.findById(id).map { maybePeople =>
+          maybePeople.map( people => Ok( Peoples.peopleWriter.writes(people) ) ).getOrElse( NotFound )
        }
     }
   }
 
-  def create(name: String, surname: String) = Action {
+  def create() = Action { implicit request =>
     Async {
-      val people = People(UUID.randomUUID().toString, name, surname)
-      PeopleController.save(people).map { status =>
-        index()
-      }
+      Peoples.peopleForm.bindFromRequest.fold(
+        errors => Future(BadRequest("Not good !!!!")),
+        people => {
+          val peopleWithID = people.copy(id = Some(UUID.randomUUID().toString))
+          Peoples.save(peopleWithID).map { status =>
+            Ok( Json.obj( "success" -> status.isSuccess,"message" -> status.getMessage, "people" -> Peoples.peopleWriter.writes(people) ) )
+          }
+        }
+      )
     }
   }
 
   def delete(id: String) = Action {
     Async {
-      PeopleController.remove(id).map { status =>
-        index()
+      Peoples.remove(id).map { status =>
+        Ok( Json.obj( "success" -> status.isSuccess,"message" -> status.getMessage) )
       }
     }
   }
-
 }
--- samples/scala/app/models/People.scala
+++ samples/scala/app/models/People.scala
@@ -1,12 +1,55 @@
 package models
 
-/**
- * Created with IntelliJ IDEA.
- * User: mathieuancelin
- * Date: 29/05/13
- * Time: 18:30
- * To change this template use File | Settings | File Templates.
- */
-class People {
+import play.api.libs.json.Json
+import org.ancelin.play2.couchbase.Couchbase
+import org.ancelin.play2.couchbase.Couchbase._
+import com.couchbase.client.protocol.views.{ComplexKey, Query}
+import net.spy.memcached.ops.OperationStatus
+import scala.concurrent.Future
+import play.api.Play.current
+import play.api.data.Form
+import play.api.data._
+import play.api.data.Forms._
+import play.api.data.validation.Constraints._
+import net.spy.memcached.PersistTo
 
+case class People(id: Option[String], name: String, surname: String)
+
+object Peoples {
+
+  implicit val peopleReader = Json.reads[People]
+  implicit val peopleWriter = Json.writes[People]
+  implicit val ec = Couchbase.couchbaseExecutor
+  implicit val client = Couchbase.bucket("people").client.getOrElse(throw new RuntimeException("Ooops !!!"))
+
+  val peopleForm: Form[People] = Form(
+    mapping(
+      "id" -> optional(text),
+      "name" -> nonEmptyText,
+      "surname" -> nonEmptyText
+    )(People.apply)(People.unapply)
+  )
+
+  def findById(id: String): Future[Option[People]] = {
+    get[People](id)
+  }
+
+  def findAll(): Future[List[People]] = {
+    find[People]("people", "by_name")(new Query().setIncludeDocs(true))
+  }
+
+  def findByName(name: String): Future[Option[People]] = {
+    val query = new Query().setIncludeDocs(true).setLimit(1)
+      .setRangeStart(ComplexKey.of(name))
+      .setRangeEnd(ComplexKey.of(s"$name\uefff"))
+    find[People]("people", "by_name")(query).map(_.headOption)
+  }
+
+  def save(people: People): Future[OperationStatus] = {
+    set[People]( people.id.get, people, PersistTo.ONE )
+  }
+
+  def remove(id: String): Future[OperationStatus] = {
+    delete(id)
+  }
 }
