3f305c91239a459dbb95f2de485bc7229a5e4e2b
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2013-06-07 10:11:08 +0200

    Add sync lib script

---
+++ sync.sh
@@ -0,0 +1,4 @@
+#sbt publish-local;
+cp -rvf /Users/mathieuancelin/.ivy2/local/org.ancelin.play2.couchbase /Users/mathieuancelin/.ivy2/cache/
+cp -rvf /Users/mathieuancelin/.ivy2/local/org.ancelin.play2.couchbase /Users/mathieuancelin/tools/play-2.1.1/repository/cache/
+cp -rvf /Users/mathieuancelin/.ivy2/local/org.ancelin.play2.couchbase /Users/mathieuancelin/tools/play-2.1.1/repository/local/
