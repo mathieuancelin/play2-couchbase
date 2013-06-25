4a2022c5aac15000dba3d747dcf1b834547f3b65
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2013-06-25 11:36:42 +0200

    Add Cache plugin based on Couchbase

--- README.md
+++ README.md
@@ -488,6 +488,36 @@ It's also not a perfect solution, because the polling can induce some overhead f
 Let's hope Couchbase guys will introduce some kind of custom completable Java future ;-)
 
 
+Use Couchbase as Cache implementation
+=====================================
+
+in `conf/play.plugins` file and add :
+`5000:org.ancelin.play2.couchbase.CouchbaseCachePlugin`
+
+then you need to add the following to your configuration
+
+```
+couchbase.cache.bucket="name of the bucket to use"
+couchbase.cache.enable=true
+```
+
+you can also specify a common namespace for the cache keys with
+
+
+```
+couchbase.cache.namespace="play-cache."
+```
+
+then you can use your Play Cache the standard way
+
+```scala
+
+Cache.set("item.key", connectedUser)
+val maybeUser: Option[User] = Cache.getAs[User]("item.key")
+
+
+```
+
 Git
 ===================================
 
---
+++ plugin/src/main/scala/org/ancelin/play2/couchbase/CouchbaseCachePlugin.scala
@@ -0,0 +1,70 @@
+package org.ancelin.play2.couchbase
+
+import play.api.cache.{CacheAPI, CachePlugin}
+import play.api.{PlayException, Logger, Plugin, Application}
+import java.util.concurrent.TimeUnit
+import net.spy.memcached.transcoders.{Transcoder, SerializingTranscoder}
+import java.io.{ObjectOutputStream, ByteArrayOutputStream, ObjectStreamClass}
+
+class CouchbaseCachePlugin(app: Application) extends CachePlugin {
+
+  lazy val api = new CacheAPI {
+
+    class CustomSerializing extends SerializingTranscoder {
+
+      override protected def deserialize(data: Array[Byte]): java.lang.Object = {
+        new java.io.ObjectInputStream(new java.io.ByteArrayInputStream(data)) {
+          override protected def resolveClass(desc: ObjectStreamClass) = {
+            Class.forName(desc.getName(), false, play.api.Play.current.classloader)
+          }
+        }.readObject()
+      }
+
+      override protected def serialize(obj: java.lang.Object) = {
+        val bos: ByteArrayOutputStream = new ByteArrayOutputStream()
+        new ObjectOutputStream(bos).writeObject(obj)
+        bos.toByteArray()
+      }
+    }
+
+    lazy val tc = new CustomSerializing().asInstanceOf[Transcoder[Any]]
+
+    def get(key: String) = {
+      val future = client.asyncGet(namespace + key, tc)
+      try {
+        val any = future.get(1, TimeUnit.SECONDS)
+        Option(
+          any match {
+            case x: java.lang.Byte => x.byteValue()
+            case x: java.lang.Short => x.shortValue()
+            case x: java.lang.Integer => x.intValue()
+            case x: java.lang.Long => x.longValue()
+            case x: java.lang.Float => x.floatValue()
+            case x: java.lang.Double => x.doubleValue()
+            case x: java.lang.Character => x.charValue()
+            case x: java.lang.Boolean => x.booleanValue()
+            case x => x
+          }
+        )
+      } catch {
+        case e: Throwable =>
+          future.cancel(false)
+          None
+      }
+    }
+
+    def set(key: String, value: Any, expiration: Int) {
+      client.set(namespace + key, expiration, value, tc)
+    }
+
+    def remove(key: String) {
+      client.delete(namespace + key)
+    }
+  }
+  lazy val client = Couchbase.client( app.configuration.getString("couchbase.cache.bucket").getOrElse(throw new PlayException("Unspecified bucket", "You have to specify a bucket to use Couchbase as a cache.")))(app)
+  lazy val namespace: String = app.configuration.getString("couchbase.cache.namespace").getOrElse("")
+
+  override lazy val enabled = {
+    app.configuration.getBoolean("couchbase.cache.enable").getOrElse(false)
+  }
+}
