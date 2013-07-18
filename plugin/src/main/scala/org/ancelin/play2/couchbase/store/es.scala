1cb7077ec227fca91c9bd7accd6961e40d9b6986
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2013-07-18 21:10:34 +0200

    Full JSON serialization

--- plugin/src/main/scala/org/ancelin/play2/couchbase/store/es.scala
+++ plugin/src/main/scala/org/ancelin/play2/couchbase/store/es.scala
@@ -5,11 +5,12 @@ import java.util.concurrent.{TimeUnit, ConcurrentHashMap}
 import org.ancelin.play2.couchbase.{Constants, Couchbase, CouchbaseBucket}
 import java.io.{ObjectOutputStream, ByteArrayOutputStream, ObjectStreamClass}
 import scala.concurrent.{Await, ExecutionContext}
-import play.api.libs.json.{JsValue, Format, Json}
+import play.api.libs.json.{JsSuccess, JsValue, Format, Json}
 import play.api.Play
 import com.couchbase.client.protocol.views.{ComplexKey, Stale, Query}
 import net.spy.memcached.transcoders.{Transcoder, SerializingTranscoder}
 import scala.concurrent.duration.Duration
+import scala.reflect.ClassTag
 
 case class Message(payload: Any, eventId: Long = 0L, aggregateId: Long = 0L, timestamp: Long = System.currentTimeMillis(), version: Int = 0)
 case class CouchbaseMessage(messageKey: String, blobKey: String, eventId: Long = 0L, aggregateId: Long = 0L, timestamp: Long = System.currentTimeMillis(), version: Int = 0, datatype: String = "eventsourcing-message", blobClass: String, blob: JsValue)
@@ -21,7 +22,7 @@ object Message {
   def create(payload: Any, id: Long, aggregate: Long, version: Int) = Message(payload, id, aggregate, System.currentTimeMillis(), version)
 }
 
-case class WriteInJournal(message: Message, replyTo: ActorRef)
+case class WriteInJournal(message: Message, replyTo: ActorRef, journal: CouchbaseJournal)
 case class WrittenInJournal(message: Message)
 case class Replay(message: Message)
 
@@ -31,7 +32,7 @@ trait EventStored extends Actor {
 
   abstract override def receive = {
     case msg: Message => {
-      couchbaseJournal.journal.forward(WriteInJournal(msg, self))
+      couchbaseJournal.journal.forward(WriteInJournal(msg, self, couchbaseJournal))
     }
     case WrittenInJournal(msg) => {
       super.receive(msg)
@@ -50,14 +51,15 @@ trait EventStored extends Actor {
 
 class CouchbaseJournalActor(bucket: CouchbaseBucket, format: Format[CouchbaseMessage], ec: ExecutionContext) extends Actor {
   def receive = {
-    case WriteInJournal(msg, to) => {
+    case WriteInJournal(msg, to, journal) => {
       val blobKey = s"eventsourcing-message-${msg.eventId}-${msg.aggregateId}-${msg.timestamp}-blob"
       val dataKey = s"eventsourcing-message-${msg.eventId}-${msg.aggregateId}-${msg.timestamp}-data"
-      val blobAsJson = Json.parse(play.libs.Json.stringify(play.libs.Json.toJson(msg.payload)))
-      val dataMsg = CouchbaseMessage(dataKey, blobKey, msg.eventId, msg.aggregateId, msg.timestamp, msg.version, "eventsourcing-message", msg.payload.getClass.getName, blobAsJson)
-      Couchbase.set(dataKey, dataMsg)(bucket, format, ec)
-        .flatMap(_ => BlobRepo.set(bucket, ec, blobKey, msg.payload))(ec)
-        .map(_ => to ! WrittenInJournal(msg))(ec)
+      journal.formatters.get(msg.payload.getClass.getName).map { formatter =>
+        val blobAsJson = formatter.asInstanceOf[Format[Any]].writes(msg.payload)
+        val dataMsg = CouchbaseMessage(dataKey, blobKey, msg.eventId, msg.aggregateId, msg.timestamp, msg.version, "eventsourcing-message", msg.payload.getClass.getName, blobAsJson)
+        Couchbase.set(dataKey, dataMsg)(bucket, format, ec)
+          .map(_ => to ! WrittenInJournal(msg))(ec)
+      }.getOrElse(throw new RuntimeException(s"Can't find formatter for class ${msg.payload.getClass.getName}"))
     }
     case _ =>
   }
@@ -67,110 +69,77 @@ class CouchbaseJournal(system: ActorSystem, bucket: CouchbaseBucket, format: For
   implicit val ec = Couchbase.couchbaseExecutor(Play.current)
   val journal: ActorRef = system.actorOf(Props(new CouchbaseJournalActor(bucket, format, ec)))
   var actors: List[ActorRef] = List[ActorRef]()
-  val byDataType = Couchbase.view("event-sourcing", "datatype")(bucket, ec)
-  val byTimestamp = Couchbase.view("event-sourcing", "by_timestamp")(bucket, ec)
-  val byEventId = Couchbase.view("event-sourcing", "by_eventid")(bucket, ec)
-  val byAggregateId = Couchbase.view("event-sourcing", "by_aggregateid")(bucket, ec)
-  val byVersion = Couchbase.view("event-sourcing", "by_version")(bucket, ec)
+  private val byDataType = Couchbase.view("event-sourcing", "datatype")(bucket, ec)
+  private val byTimestamp = Couchbase.view("event-sourcing", "by_timestamp")(bucket, ec)
+  private val byEventId = Couchbase.view("event-sourcing", "by_eventid")(bucket, ec)
+  private val byAggregateId = Couchbase.view("event-sourcing", "by_aggregateid")(bucket, ec)
+  private val byVersion = Couchbase.view("event-sourcing", "by_version")(bucket, ec)
 
-  val all = new Query().setIncludeDocs(true).setStale(Stale.FALSE).setDescending(false)
-  def allFrom(from: Long) = new Query().setIncludeDocs(true).setStale(Stale.FALSE).setDescending(false).setRangeStart(ComplexKey.of(from.asInstanceOf[AnyRef])).setRangeEnd(ComplexKey.of(Long.MaxValue.asInstanceOf[AnyRef]))
-  def allUntil(to: Long) = new Query().setIncludeDocs(true).setStale(Stale.FALSE).setDescending(false).setRangeStart(ComplexKey.of(0L.asInstanceOf[AnyRef])).setRangeEnd(ComplexKey.of(to.asInstanceOf[AnyRef]))
+  private val all = new Query().setIncludeDocs(true).setStale(Stale.FALSE).setDescending(false)
+  private def allFrom(from: Long) = new Query().setIncludeDocs(true).setStale(Stale.FALSE).setDescending(false).setRangeStart(ComplexKey.of(from.asInstanceOf[AnyRef])).setRangeEnd(ComplexKey.of(Long.MaxValue.asInstanceOf[AnyRef]))
+  private def allUntil(to: Long) = new Query().setIncludeDocs(true).setStale(Stale.FALSE).setDescending(false).setRangeStart(ComplexKey.of(0L.asInstanceOf[AnyRef])).setRangeEnd(ComplexKey.of(to.asInstanceOf[AnyRef]))
+
+  var formatters: Map[String, Format[_]] = Map()
+
+  def registerFormatter[T](formatter: Format[T])(implicit tag: ClassTag[T]) = {
+    formatters = formatters + (tag.runtimeClass.getName -> formatter)
+    this
+  }
 
   def processorOf(props: Props): ActorRef = {
     val actorRef = system.actorOf(props)
     actors = actors :+ actorRef
     actorRef
   }
+
   def processorOf(props: Props, name: String): ActorRef = {
     val actorRef = system.actorOf(props, name)
     actors = actors :+ actorRef
     actorRef
   }
 
+  private def replayMessage(message: CouchbaseMessage) = {
+    formatters.get(message.blobClass).map { formatter =>
+      formatter.reads(message.blob) match {
+        case s: JsSuccess[_] => {
+          val msg = Message(s.get, message.eventId, message.aggregateId, message.timestamp, message.version)
+          actors.foreach { actor =>
+            actor ! Replay(msg)
+          }
+        }
+        case _ => throw new RuntimeException(s"Can't read blob for class ${message.blobClass} : ${Json.stringify(message.blob)}")
+      }
+    }.getOrElse(throw new RuntimeException(s"Can't find formatter for class ${message.blobClass}"))
+  }
+
   def replayAll() = {
     byTimestamp.flatMap { view =>
       Couchbase.find[CouchbaseMessage](view)(all)(bucket, format, ec)
-    }.map { messages =>
-      messages.map { message =>
-        BlobRepo.get(bucket, ec, message.blobKey).map { maybeBlob =>
-          maybeBlob.map { blob =>
-            val msg = Message(blob, message.eventId, message.aggregateId, message.timestamp, message.version)
-            actors.foreach { actor =>
-              actor ! Replay(msg)
-            }
-          }
-        }
-      }
-    }
+    }.map(_.map(replayMessage))
   }
 
   def replayFrom(timestamp: Long) = {
     byTimestamp.flatMap { view =>
       Couchbase.find[CouchbaseMessage](view)(allFrom(timestamp))(bucket, format, ec)
-    }.map { messages =>
-      messages.map { message =>
-        BlobRepo.get(bucket, ec, message.blobKey).map { maybeBlob =>
-          maybeBlob.map { blob =>
-            val msg = Message(blob, message.eventId, message.aggregateId, message.timestamp, message.version)
-            actors.foreach { actor =>
-              actor ! Replay(msg)
-            }
-          }
-        }
-      }
-    }
+    }.map(_.map(replayMessage))
   }
 
   def replayUntil(timestamp: Long) = {
     byTimestamp.flatMap { view =>
       Couchbase.find[CouchbaseMessage](view)(allUntil(timestamp))(bucket, format, ec)
-    }.map { messages =>
-      messages.map { message =>
-        BlobRepo.get(bucket, ec, message.blobKey).map { maybeBlob =>
-          maybeBlob.map { blob =>
-            val msg = Message(blob, message.eventId, message.aggregateId, message.timestamp, message.version)
-            actors.foreach { actor =>
-              actor ! Replay(msg)
-            }
-          }
-        }
-      }
-    }
+    }.map(_.map(replayMessage))
   }
 
   def replayFromId(id: Long) = {
     byEventId.flatMap { view =>
       Couchbase.find[CouchbaseMessage](view)(allFrom(id))(bucket, format, ec)
-    }.map { messages =>
-      messages.map { message =>
-        BlobRepo.get(bucket, ec, message.blobKey).map { maybeBlob =>
-          maybeBlob.map { blob =>
-            val msg = Message(blob, message.eventId, message.aggregateId, message.timestamp, message.version)
-            actors.foreach { actor =>
-              actor ! Replay(msg)
-            }
-          }
-        }
-      }
-    }
+    }.map(_.map(replayMessage))
   }
 
   def replayUntilId(id: Long) = {
     byEventId.flatMap { view =>
       Couchbase.find[CouchbaseMessage](view)(allUntil(id))(bucket, format, ec)
-    }.map { messages =>
-      messages.map { message =>
-        BlobRepo.get(bucket, ec, message.blobKey).map { maybeBlob =>
-          maybeBlob.map { blob =>
-            val msg = Message(blob, message.eventId, message.aggregateId, message.timestamp, message.version)
-            actors.foreach { actor =>
-              actor ! Replay(msg)
-            }
-          }
-        }
-      }
-    }
+    }.map(_.map(replayMessage))
   }
 }
 
@@ -215,6 +184,17 @@ object CouchbaseJournal {
   }
 }
 
+/*
+
+BlobRepo.get(bucket, ec, message.blobKey).map { maybeBlob =>
+  maybeBlob.map { blob =>
+    val msg = Message(blob, message.eventId, message.aggregateId, message.timestamp, message.version)
+    actors.foreach { actor =>
+      actor ! Replay(msg)
+    }
+  }
+}
+
 private object MessageSerializer extends SerializingTranscoder {
 
   override protected def deserialize(data: Array[Byte]): java.lang.Object = {
@@ -244,4 +224,5 @@ private object BlobRepo {
     Couchbase.set(key, value, tc)(bucket, ec)
   }
 }
+*/
 
