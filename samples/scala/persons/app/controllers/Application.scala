4470c85619871bae8c318d4d2395ba2b8e4c84a9
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2013-07-26 17:57:25 +0200

    Add test routes

--- samples/scala/persons/app/controllers/Application.scala
+++ samples/scala/persons/app/controllers/Application.scala
@@ -1,9 +1,68 @@
 package controllers
 
 import play.api.mvc._
+import org.ancelin.play2.couchbase.Couchbase
+import play.api.Play
+import java.util.UUID
+import scala.concurrent.Future
+import play.api.libs.json.{JsObject, Json}
+import org.ancelin.play2.couchbase.CouchbaseRWImplicits.jsObjectToDocumentWriter
+import java.util.concurrent.atomic.AtomicLong
 
 object Application extends Controller {
+
+  val bucket = Couchbase.bucket("persons")(Play.current)
+  implicit val ec = Couchbase.couchbaseExecutor(Play.current)
+
   def index = Action {
     Ok(views.html.index())
+  }
+
+  def createNoPluginJson = Action(parse.json) { request =>
+    Async {
+      Future {
+        val start = System.currentTimeMillis()
+        bucket.couchbaseClient.set(UUID.randomUUID().toString, Json.stringify(request.body)).get()
+        counter.incrementAndGet()
+        adder.addAndGet(System.currentTimeMillis() - start)
+        if ( adder.get() % 500 == 0 ) {
+          println(s"average ${adder.get() / counter.get()} ms.")
+        }
+        Ok("")
+      }
+    }
+  }
+
+  def createNoPluginText = Action(parse.text) { request =>
+    Async {
+      Future {
+        val start = System.currentTimeMillis()
+        bucket.couchbaseClient.set(UUID.randomUUID().toString, request.body).get()
+        counter.incrementAndGet()
+        adder.addAndGet(System.currentTimeMillis() - start)
+        if ( adder.get() % 500 == 0 ) {
+          println(s"average ${adder.get() / counter.get()} ms.")
+        }
+        Ok("")
+      }
+    }
+  }
+
+  val counter = new AtomicLong(0L)
+  val adder = new AtomicLong(0L)
+
+  def createPluginJson = Action(parse.json) { request =>
+    Async {
+      val start = System.currentTimeMillis()
+      Couchbase.set(UUID.randomUUID().toString, request.body.as[JsObject])(bucket, jsObjectToDocumentWriter, ec).map {
+        _ =>
+          counter.incrementAndGet()
+          adder.addAndGet(System.currentTimeMillis() - start)
+          if ( adder.get() % 500 == 0 ) {
+            println(s"average ${adder.get() / counter.get()} ms.")
+          }
+          Ok("")
+      }
+    }
   }
 }
