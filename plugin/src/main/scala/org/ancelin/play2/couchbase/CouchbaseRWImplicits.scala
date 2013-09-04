82883e0666f0f1f25c3de07f92d7ab3408e03d60
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2013-09-04 22:21:33 +0200

    Add implicit RW

--- plugin/src/main/scala/org/ancelin/play2/couchbase/CouchbaseRWImplicits.scala
+++ plugin/src/main/scala/org/ancelin/play2/couchbase/CouchbaseRWImplicits.scala
@@ -11,10 +11,16 @@ object CouchbaseRWImplicits {
   implicit val documentAsJsObjectReader = new Reads[JsObject] {
     def reads(json: JsValue): JsResult[JsObject] = JsSuccess(json.as[JsObject])
   }
+  implicit val documentAsJsValuetReader = new Reads[JsValue] {
+    def reads(json: JsValue): JsResult[JsValue] = JsSuccess(json)
+  }
   implicit val stringToDocumentWriter = new Writes[String] {
     def writes(o: String): JsValue = Json.parse(o)
   }
   implicit val jsObjectToDocumentWriter = new Writes[JsObject] {
     def writes(o: JsObject): JsValue = o
+  }
+  implicit val jsValueToDocumentWriter = new Writes[JsValue] {
+    def writes(o: JsValue): JsValue = o
   }
 }
