09e476a8cf13a2b3b333523affe507874ae7d377
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2013-09-14 12:11:02 +0200

    Add highly experimental N1QL support

---
+++ plugin/src/main/scala/org/ancelin/play2/couchbase/plugins/CouchbaseN1QLPlugin.scala
@@ -0,0 +1,70 @@
+package org.ancelin.play2.couchbase.plugins
+
+import play.api.{PlayException, Plugin, Application, Play}
+import play.api.Play.current
+import play.api.libs.ws.WS
+import scala.concurrent.{ExecutionContext, Future}
+import play.api.libs.iteratee.{Enumeratee, Iteratee, Concurrent, Enumerator}
+import play.api.libs.json._
+import play.api.libs.json.JsArray
+import play.api.libs.ws.WS.WSRequestHolder
+import play.api.libs.json.JsObject
+import scala.Some
+
+class CouchbaseN1QLPlugin(app: Application) extends Plugin {
+
+  var queryBase: Option[WSRequestHolder] = None
+
+  override def onStart {
+    val host = Play.configuration.getString("couchbase.n1ql.host").getOrElse(throw new PlayException("Cannot find N1QL host", "Cannot find N1QL host in couchbase.n1ql conf."))
+    val port = Play.configuration.getString("couchbase.n1ql.port").getOrElse(throw new PlayException("Cannot find N1QL port", "Cannot find N1QL port in couchbase.n1ql conf."))
+    queryBase = Some(WS.url(s"http://${host}:${port}/query"))
+  }
+}
+
+class N1QLQuery(query: String, base: WSRequestHolder) {
+  /*def on(arg: (String, String)): N1QLQuery = {
+    new N1QLQuery(query, base)
+  }*/
+  def enumerateJson(implicit ec: ExecutionContext): Future[Enumerator[JsObject]] = {
+    toJsArray(ec).map { arr =>
+      Enumerator.enumerate(arr.value) &> Enumeratee.map[JsValue](_.as[JsObject])
+    }
+  }
+  def enumerate[T](implicit r: Reads[T], ec: ExecutionContext): Future[Enumerator[T]] = {
+    enumerateJson(ec).map { e =>
+      e &> Enumeratee.map[JsObject](r.reads(_)) &> Enumeratee.collect[JsResult[T]] { case JsSuccess(value, _) => value }
+    }
+  }
+  def asJsonEnumerator(implicit ec: ExecutionContext): Enumerator[JsObject] = {
+    Concurrent.unicast[JsObject](onStart = c => enumerateJson(ec).map(_(Iteratee.foreach[JsObject](c.push).mapDone(_ => c.eofAndEnd()))))
+  }
+  def asEnumerator[T](implicit r: Reads[T], ec: ExecutionContext): Enumerator[T] = {
+    Concurrent.unicast[T](onStart = c => enumerate[T](r, ec).map(_(Iteratee.foreach[T](c.push).mapDone(_ => c.eofAndEnd()))))
+  }
+
+  def toJsArray(implicit ec: ExecutionContext): Future[JsArray] = {
+    base.post(Map("q" -> Seq(query))).map { response =>
+      (response.json \ "resultset").as[JsArray]
+    }
+  }
+  def toList[T](implicit r: Reads[T], ec: ExecutionContext): Future[List[T]] = {
+    enumerate[T](r, ec).flatMap(_(Iteratee.getChunks[T]).flatMap(_.run))
+  }
+
+  def headOption[T](implicit r: Reads[T], ec: ExecutionContext): Future[Option[T]] = {
+    enumerate[T](r, ec).flatMap(_(Iteratee.head[T]).flatMap(_.run))
+  }
+}
+
+object CouchbaseN1QLPlugin {
+
+  lazy val N1QLPlugin = Play.current.plugin[CouchbaseN1QLPlugin] match {
+    case Some(plugin) => plugin
+    case _ => throw new PlayException("CouchbaseN1QLPlugin Error", "Cannot find an instance of CouchbaseN1QLPlugin.")
+  }
+
+  def N1QL(query: String): N1QLQuery = {
+    new N1QLQuery(query, N1QLPlugin.queryBase.get)
+  }
+}
