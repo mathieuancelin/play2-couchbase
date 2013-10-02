8ff2166c5ca5634bf5c6a51ab1878d1ad6e4a71a
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2013-10-02 20:27:15 +0200

    Update script

--- sync.sh
+++ sync.sh
@@ -2,3 +2,5 @@
 cp -rvf /Users/mathieuancelin/.ivy2/local/org.ancelin.play2.couchbase /Users/mathieuancelin/.ivy2/cache/
 cp -rvf /Users/mathieuancelin/.ivy2/local/org.ancelin.play2.couchbase /Users/mathieuancelin/tools/play-2.1.1/repository/cache/
 cp -rvf /Users/mathieuancelin/.ivy2/local/org.ancelin.play2.couchbase /Users/mathieuancelin/tools/play-2.1.1/repository/local/
+cp -rvf /Users/mathieuancelin/.ivy2/local/org.ancelin.play2.couchbase /Users/mathieuancelin/tools/play-2.2.0/repository/cache/
+cp -rvf /Users/mathieuancelin/.ivy2/local/org.ancelin.play2.couchbase /Users/mathieuancelin/tools/play-2.2.0/repository/local/
