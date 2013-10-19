a5643203938c77b48d98417efb8f2d24e5dbeb50
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2013-10-19 21:34:07 +0200

    Add logs

--- plugin/src/main/scala/org/ancelin/play2/couchbase/CouchbasePlugin.scala
+++ plugin/src/main/scala/org/ancelin/play2/couchbase/CouchbasePlugin.scala
@@ -8,6 +8,7 @@ class CouchbasePlugin(implicit app: Application) extends Plugin {
   val logger = Logger("CouchbasePlugin")
   var buckets: Map[String, CouchbaseBucket] = Map[String, CouchbaseBucket]()
   override def onStart {
+    logger.info("Starting Couchbase plugin ...")
     play.api.Play.configuration(app).getObjectList("couchbase.buckets").map { configs =>
       configs.foreach(connect)
       configs
@@ -16,6 +17,7 @@ class CouchbasePlugin(implicit app: Application) extends Plugin {
       val cl: CouchbaseBucket = Couchbase().connect()
       buckets = buckets + (cl.bucket -> cl)
     }
+    logger.info("Starting Couchbase plugin done, have fun !!!")
   }
   private def connect(config: ConfigObject) {
     val bucket = config.get("bucket").unwrapped().asInstanceOf[String]
@@ -29,12 +31,14 @@ class CouchbasePlugin(implicit app: Application) extends Plugin {
     val pass = config.get("pass").unwrapped().asInstanceOf[String]
     val timeout = config.get("timeout").unwrapped().asInstanceOf[String].toLong
     val couchbase: CouchbaseBucket = Couchbase(hosts.toList, port, base, bucket, user, pass, timeout)
-    logger.info(s"Connection to bucket $bucket ...")
+    logger.info(s"""Connection to bucket "${bucket}" ...""")
     buckets = buckets + (bucket -> couchbase.connect())
   }
   override def onStop {
-    logger.info("Couchbase shutdown")
+    new Throwable().printStackTrace()
+    logger.info("Couchbase plugin shutdown, disconnecting all buckets ...")
     buckets.foreach { tuple => tuple._2.disconnect() }
     buckets = buckets.empty
+    logger.info("Couchbase plugin shutdown done.")
   }
 }
--- plugin/src/main/scala/org/ancelin/play2/couchbase/client/CouchbaseFutures.scala
+++ plugin/src/main/scala/org/ancelin/play2/couchbase/client/CouchbaseFutures.scala
@@ -4,6 +4,7 @@ import net.spy.memcached.internal._
 import scala.concurrent.{Promise, Future, ExecutionContext}
 import com.couchbase.client.internal.{HttpCompletionListener, HttpFuture}
 import net.spy.memcached.ops.OperationStatus
+import play.Logger
 
 object CouchbaseFutures {
 
@@ -11,13 +12,14 @@ object CouchbaseFutures {
     val promise = Promise[java.util.Map[String, AnyRef]]()
     future.addListener(new BulkGetCompletionListener() {
       def onComplete(f: BulkGetFuture[_]) = {
-        if (f.isDone || f.isCancelled || f.isTimeout) {
-          if (Constants.failWithOpStatus && (!f.getStatus.isSuccess)) {
-            promise.failure(new OperationFailedException(f.getStatus))
-          } else {
+        if (Constants.failWithOpStatus && (!f.getStatus.isSuccess)) {
+          promise.failure(new OperationFailedException(f.getStatus))
+        } else {
+          if (!f.getStatus.isSuccess) Logger.error(f.getStatus.getMessage)
+          if (f.isDone || f.isCancelled || f.isTimeout) {
             promise.success(f.get().asInstanceOf[java.util.Map[String, AnyRef]]);
-          }
-        } else promise.failure(new Throwable(s"ListenableFuture epic fail !!! ${f.isDone} : ${f.isCancelled} : ${f.isTimeout}"))
+          } else promise.failure(new Throwable(s"ListenableFuture epic fail !!! ${f.isDone} : ${f.isCancelled} : ${f.isTimeout}"))
+        }
       }
     })
     promise.future
@@ -27,13 +29,14 @@ object CouchbaseFutures {
     val promise = Promise[T]()
     future.addListener(new GetCompletionListener() {
       def onComplete(f: GetFuture[_]) = {
-        if (f.isDone || f.isCancelled) {
-          if (Constants.failWithOpStatus && (!f.getStatus.isSuccess)) {
-            promise.failure(new OperationFailedException(f.getStatus))
-          } else {
+        if (Constants.failWithOpStatus && (!f.getStatus.isSuccess)) {
+          promise.failure(new OperationFailedException(f.getStatus))
+        } else {
+          if (!f.getStatus.isSuccess) Logger.error(f.getStatus.getMessage)
+          if (f.isDone || f.isCancelled) {
             promise.success(f.get().asInstanceOf[T]);
-          }
-        } else promise.failure(new Throwable(s"ListenableFuture epic fail !!! ${f.isDone} : ${f.isCancelled}"))
+          } else promise.failure(new Throwable(s"ListenableFuture epic fail !!! ${f.isDone} : ${f.isCancelled}"))
+        }
       }
     })
     promise.future
@@ -43,13 +46,14 @@ object CouchbaseFutures {
     val promise = Promise[OperationStatus]()
     future.addListener(new HttpCompletionListener() {
       def onComplete(f: HttpFuture[_]) = {
-        if (f.isDone || f.isCancelled) {
-          if (Constants.failWithOpStatus && (!f.getStatus.isSuccess)) {
-            promise.failure(new OperationFailedException(f.getStatus))
-          } else {
+        if (Constants.failWithOpStatus && (!f.getStatus.isSuccess)) {
+          promise.failure(new OperationFailedException(f.getStatus))
+        } else {
+          if (!f.getStatus.isSuccess) Logger.error(f.getStatus.getMessage)
+          if (f.isDone || f.isCancelled) {
             promise.success(f.getStatus);
-          }
-        } else promise.failure(new Throwable(s"ListenableFuture epic fail !!! ${f.isDone} : ${f.isCancelled}"))
+          } else promise.failure(new Throwable(s"ListenableFuture epic fail !!! ${f.isDone} : ${f.isCancelled}"))
+        }
       }
     })
     promise.future
@@ -59,13 +63,14 @@ object CouchbaseFutures {
     val promise = Promise[T]()
     future.addListener(new HttpCompletionListener() {
       def onComplete(f: HttpFuture[_]) = {
-        if (f.isDone || f.isCancelled) {
-          if (Constants.failWithOpStatus && (!f.getStatus.isSuccess)) {
-            promise.failure(new OperationFailedException(f.getStatus))
-          } else {
+        if (Constants.failWithOpStatus && (!f.getStatus.isSuccess)) {
+          promise.failure(new OperationFailedException(f.getStatus))
+        } else {
+          if (!f.getStatus.isSuccess) Logger.error(f.getStatus.getMessage)
+          if (f.isDone || f.isCancelled) {
             promise.success(f.get().asInstanceOf[T]);
-          }
-        } else promise.failure(new Throwable(s"ListenableFuture epic fail !!! ${f.isDone} : ${f.isCancelled}"))
+          } else promise.failure(new Throwable(s"ListenableFuture epic fail !!! ${f.isDone} : ${f.isCancelled}"))
+        }
       }
     })
     promise.future
@@ -75,13 +80,14 @@ object CouchbaseFutures {
     val promise = Promise[OperationStatus]()
     future.addListener(new OperationCompletionListener() {
       def onComplete(f: OperationFuture[_]) = {
-        if (f.isDone || f.isCancelled) {
-          if (Constants.failWithOpStatus && (!f.getStatus.isSuccess)) {
-            promise.failure(new OperationFailedException(f.getStatus))
-          } else {
+        if (Constants.failWithOpStatus && (!f.getStatus.isSuccess)) {
+          promise.failure(new OperationFailedException(f.getStatus))
+        } else {
+          if (!f.getStatus.isSuccess) Logger.error(f.getStatus.getMessage)
+          if (f.isDone || f.isCancelled) {
             promise.success(f.getStatus);
-          }
-        } else promise.failure(new Throwable(s"ListenableFuture epic fail !!! ${f.isDone} : ${f.isCancelled}"))
+          } else promise.failure(new Throwable(s"ListenableFuture epic fail !!! ${f.isDone} : ${f.isCancelled}"))
+        }
       }
     })
     promise.future
@@ -91,13 +97,14 @@ object CouchbaseFutures {
     val promise = Promise[T]()
     future.addListener(new OperationCompletionListener() {
       def onComplete(f: OperationFuture[_]) = {
-        if (f.isDone || f.isCancelled) {
-          if (Constants.failWithOpStatus && (!f.getStatus.isSuccess)) {
-            promise.failure(new OperationFailedException(f.getStatus))
-          } else {
+        if (Constants.failWithOpStatus && (!f.getStatus.isSuccess)) {
+          promise.failure(new OperationFailedException(f.getStatus))
+        } else {
+          if (!f.getStatus.isSuccess) Logger.error(f.getStatus.getMessage)
+          if (f.isDone || f.isCancelled) {
             promise.success(f.get().asInstanceOf[T]);
-          }
-        } else promise.failure(new Throwable(s"ListenableFuture epic fail !!! ${f.isDone} : ${f.isCancelled}"))
+          } else promise.failure(new Throwable(s"ListenableFuture epic fail !!! ${f.isDone} : ${f.isCancelled}"))
+        }
       }
     })
     promise.future
