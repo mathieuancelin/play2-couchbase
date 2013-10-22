5056f266b7125c6cd047d78276d0475fc9afbf71
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2013-10-22 12:36:02 +0200

    Update test

--- samples/scala/shorturls/test/ApplicationSpec.scala
+++ samples/scala/shorturls/test/ApplicationSpec.scala
@@ -9,54 +9,46 @@ import java.util.concurrent.{Executors, CountDownLatch}
 import play.api.libs.iteratee
 import scala.concurrent.ExecutionContext
 
+import org.junit.runner._
+
+import org.specs2.mutable._
+import org.specs2.runner._
+import org.specs2.matcher._
+
+import org.junit.runner._
+
+import play.api.test._
+import play.api.test.Helpers._
+import play.api.mvc._
+import play.api.libs.json._
+import play.api.libs.json.Reads._
+import play.Logger
+
+
 /**
  * Add your spec here.
  * You can mock out a whole application including requests, plugins etc.
  * For more information, consult the wiki.
  */
+@RunWith(classOf[JUnitRunner]) 
 class ApplicationSpec extends Specification {
 
   "Application" should {
 
-    val app = FakeApplication()
-    implicit val ec = ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
-
-    "find an some urls" in {
-      running(app) {
-        println("=======================================================>   test1 ================================================")
-        val home = route(app, FakeRequest(GET, "/urls/")).get
-        val content = contentAsString(home)
-        println(content)
-        status(home) must equalTo(OK)
-
-
-        val home2 = route(app, FakeRequest(GET, "/urls/")).get
-        val content2 = contentAsString(home2)
-        println(content2)
-        status(home2) must equalTo(OK)
-
-        println("=======================================================>   fin test1 ================================================")
-        contentType(home) must beSome.which(_ == "application/json")
-      }
+    "find an some urls" in new WithApplication {
+      val home = route(FakeRequest(GET, "/")).get
+      val content = contentAsString(home)
+      println(content)
+      status(home) must equalTo(OK)
+      contentType(home) must beSome.which(_ == "application/json")
     }
 
-    "find other urls" in {
-      running(app) {
-        val latch = new CountDownLatch(1)
-        println("=======================================================>   test2 l1  ================================================")
-        route(app, FakeRequest(GET, "/urls/")).get.map { home =>
-          home.body |>>> iteratee.Iteratee.consume[Array[Byte]]().map { content =>
-            println("=======================================================>   test2 l2  ================================================")
-            println(content)
-            println("=======================================================>   test2 l3  ================================================")
-            latch.countDown()
-            println("=======================================================>   test2 l5  ================================================")
-          }
-        }
-        latch.await()
-        println("=======================================================>   fin test2   ================================================")
-        contentType(route(app, FakeRequest(GET, "/urls/")).get) must beSome.which(_ == "application/json")
-      }
+    "find an some urls 2" in new WithApplication {
+      val home = route(FakeRequest(GET, "/")).get
+      val content = contentAsString(home)
+      println(content)
+      status(home) must equalTo(OK)
+      contentType(home) must beSome.which(_ == "application/json")
     }
   }
 }
