fedcd7a09187eed089e08040acf2174f14c84f1d
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2013-12-16 19:36:07 +0100

    Add release 0.6

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "play2-couchbase"
-  val appVersion      = "0.6-SNAPSHOT"
+  val appVersion      = "0.6"
   val appScalaVersion = "2.10.2"
   val appScalaBinaryVersion = "2.10"
   val appScalaCrossVersions = Seq("2.10.2")
